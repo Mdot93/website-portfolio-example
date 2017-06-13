5aef83962a8c4bfabc23383dfd18a87b0976e6b4
ACE User <aceuser@e140-s19.lab.lagcc>
2017-06-12 21:17:12 -0400

    added scrpit for nav

--- website-portfolio/js/script.js
+++ website-portfolio/js/script.js
@@ -1,1 +1,15 @@
-console.log("Who ya wit!")
+var topNavList = document.querySelector('#top-nav-list');
+var topNavToggle = document.querySelector('#top-nav-toggle');
+var  navCollasped = true;
+
+topNavToggle.addEventListener('click',function(){
+	if (navCollasped === true){
+	    topNavList.classList.remove('nav-list-collapsed');
+	    topNavToggle.innerHTML = '<i class="fa fa-window-close" aria-hidden="true"></i>';
+	 	navCollasped = false;   
+	 } else { 
+		topNavList.class.add('nav-list-collapsed');
+		topNavToggle.innerHTML = '<i class="fa fa-window-close" aria-hidden="true"></i>';
+		navCollasped = true;
+	}
+});
